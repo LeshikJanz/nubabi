b79e4c8d30b792a54baad84636f3155a2c772b59
Adrian Perez <adrian@adrianperez.org>
2017-06-12 18:21:01 +0200

    feat(stimulation): add no-op filter button to browse activities

--- src/native/stimulation/BrowseActivitiesScreen.js
+++ src/native/stimulation/BrowseActivitiesScreen.js
@@ -1,8 +1,11 @@
 // @flow
 import type { NavigationProp, ActivityFilterInput } from 'react-navigation';
 import React, { PureComponent } from 'react';
+import { TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 import { Screen } from '../components';
 import BrowseActivities from './BrowseActivities';
+import theme from '../../common/themes/defaultTheme';
 
 type Props = {
   navigation: NavigationProp<*>,
@@ -17,6 +20,11 @@ export class BrowseActivitiesScreen extends PureComponent {
   props: Props;
   static navigationOptions = {
     title: 'Activities',
+    headerRight: (
+      <TouchableOpacity style={{ padding: 10 }}>
+        <Icon name="ios-funnel-outline" size={20} />
+      </TouchableOpacity>
+    ),
   };
 
   handleBrowseAll = () => {
