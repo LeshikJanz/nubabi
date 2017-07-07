6b85d9e1256749eceea3d6a665e5c7db978903fc
Adrian Perez <adrian@adrianperez.org>
2017-07-07 17:26:45 +0200

    fix(navigation): remove black touchable feedback from Settings

--- src/native/navigation/RightHeader.js
+++ src/native/navigation/RightHeader.js
@@ -1,25 +1,37 @@
 // @flow
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
 import NubabiIcon from '../../common/icons/nubabi';
 import { HEADER_FONT_COLOR } from '../../common/themes/defaultTheme';
 
-const RightHeader = ({ navigate }: { navigate: () => void }) =>
+type Props = {
+  navigate: string => void,
+};
+
+const RightHeader = ({ navigate }: Props) =>
   <View style={{ flexDirection: 'row' }}>
-    <NubabiIcon name="alerts" style={styles.headerIcon} />
-    <NubabiIcon
-      name="settings"
-      style={[styles.headerIcon, { fontSize: 16 }]}
+    <TouchableOpacity style={styles.headerIcon}>
+      <NubabiIcon
+        name="alerts"
+        style={{ color: HEADER_FONT_COLOR, fontSize: 17 }}
+      />
+    </TouchableOpacity>
+
+    <TouchableOpacity
+      style={styles.headerIcon}
       onPress={() => navigate('settings')}
-    />
+    >
+      <NubabiIcon
+        name="settings"
+        style={{ color: HEADER_FONT_COLOR, fontSize: 16 }}
+      />
+    </TouchableOpacity>
   </View>;
 
 const styles = StyleSheet.create({
   headerIcon: {
     padding: 10,
     backgroundColor: 'transparent',
-    color: HEADER_FONT_COLOR,
-    fontSize: 17,
   },
 });
 
