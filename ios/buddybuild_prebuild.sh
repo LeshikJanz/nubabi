dfa1346ffdf34daefab83af8d67a6ac5637e477e
Adrian Perez <adrian@adrianperez.org>
2017-07-14 11:15:34 +0200

    build(iOS): attempt to fix compilation error due to glog

--- ios/buddybuild_prebuild.sh
+++ ios/buddybuild_prebuild.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 source ../.env
 echo "Running tests..."
+
+### fix RN 0.46 error with third-party.sh script
+echo "Fixing RN 0.46 glog compilation error"
+curl https://raw.githubusercontent.com/facebook/react-native/5c53f89dd86160301feee024bce4ce0c89e8c187/scripts/ios-configure-glog.sh >ios-configure-glog.sh
+chmod +x ios-configure-glog.sh
+mv ios-configure-glog.sh node_modules/react-native/scripts/
+###
+
 echo $BUDDYBUILD_WORKSAPACE
 echo $PATH
 
