6eb7d99f08b784c43b9f3175d9d54fbe035ca891
Pavel Gorbach <polgorbach@gmail.com>
2017-11-13 19:40:02 +0300

    feat(web/memories): Added Memories component

--- platforms/web/src/web/bundles/profile/containers/Profile/Main.js
+++ platforms/web/src/web/bundles/profile/containers/Profile/Main.js
@@ -12,11 +12,48 @@ type Props = {
 
 const Memories = styled.div``;
 
+const MemoriesTitle = styled.h3`
+  font-weight: normal;
+  font-size: 18px;
+  margin: 0 0 15px;
+  color: ${props => props.theme.colors.open.black0};
+`;
+
+const MemoriesList = styled(Flex)`
+  margin: 0;
+  padding: 0;
+`;
+
+const MemoriesListItem = styled(Box)`
+  padding: 0;
+  box-shadow:  ${props => props.theme.shadows.panel};
+  background:  ${props => props.theme.colors.white};
+  border-radius: 4px;
+  margin-bottom: 30px;
+  
+  >h4 {
+    font-size: 16px;
+    font-weight: normal;
+    color: ${props => props.theme.colors.open.black0};
+    margin: 15px;
+  }
+`;
+
+const MemoriesListImage = styled.div`
+  width: 100%;
+  margin: 0;
+  padding: 0;
+  height: 160px;
+  background: url(${props => props.image});
+  background-size:cover;
+  background-position:top;
+`;
+
 class ProfileMain extends PureComponent<Props> {
   render() {
     const { memories, growth, activities, name, dob } = this.props;
 
-    console.log('activities', activities);
+    console.log('memories', memories);
 
     return (
       <Box width={4/7} is={Main} p={15} pt={25}>
@@ -25,19 +62,22 @@ class ProfileMain extends PureComponent<Props> {
         <Activities name={name} activities={activities}/>
 
         <Memories>
-          <h3>Recent Memories</h3>
-          <ul>
+          <MemoriesTitle>Recent Memories</MemoriesTitle>
+
+          <MemoriesList justify='space-between' wrap>
             {memories.edges.map(edge => {
               return (
-                <li key={edge.node.id}>
-                  <p>{edge.node.title}</p>
+                <MemoriesListItem width={4/9} m={5} key={edge.node.id}>
+
                   {edge.node.files.edges && (
-                    <img src={edge.node.files.edges[0].node.thumb.url} />
+                    <MemoriesListImage image={edge.node.files.edges[0].node.thumb.url} />
                   )}
-                </li>
+
+                  <h4>{edge.node.title}</h4>
+                </MemoriesListItem>
               );
             })}
-          </ul>
+          </MemoriesList>
         </Memories>
       </Box>
     );
