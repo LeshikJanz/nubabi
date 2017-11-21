d7f461c6c3eaf36eab54d5b9acd478626335af6d
Adrian Perez <adrian@adrianperez.org>
2017-11-21 16:37:22 +0100

    chore: adjust firebase info, CI, etc

--- platforms/firebase/functions/index.js
+++ platforms/firebase/functions/index.js
@@ -1,25 +1,27 @@
-"use strict";
 
-var _firebaseFunctions = require("firebase-functions");
 
-var _firebaseFunctions2 = _interopRequireDefault(_firebaseFunctions);
+const _firebaseFunctions = require("firebase-functions");
 
-var _curriculum = require("./curriculum");
+const functions = _interopRequireWildcard(_firebaseFunctions);
 
-var _curriculum2 = _interopRequireDefault(_curriculum);
+const _curriculum = require("./curriculum");
 
-var _imageResize = require("./imageResize");
+const _curriculum2 = _interopRequireDefault(_curriculum);
 
-var _imageResize2 = _interopRequireDefault(_imageResize);
+const _imageResize = require("./imageResize");
+
+const _imageResize2 = _interopRequireDefault(_imageResize);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-exports.startCurriculum = _firebaseFunctions2.default.database.ref("/babies/{babyId}").onWrite(_curriculum2.default.handler);
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; }  const newObj = {}; if (obj != null) { for (const key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj;  }
 
-exports.userAvatarResize = _firebaseFunctions2.default.database.ref("/users/{userId}/avatar/url").onWrite(_imageResize2.default.handler);
+exports.startCurriculum = functions.database.ref("/babies/{babyId}").onWrite(_curriculum2.default.handler);
 
-exports.babyAvatarResize = _firebaseFunctions2.default.database.ref("/babies/{babyId}/avatar/url").onWrite(_imageResize2.default.handler);
+exports.userAvatarResize = functions.database.ref("/users/{userId}/avatar/url").onWrite(_imageResize2.default.handler);
 
-exports.coverImageResize = _firebaseFunctions2.default.database.ref("/babies/{babyId}/coverImage/url").onWrite(_imageResize2.default.handler);
+exports.babyAvatarResize = functions.database.ref("/babies/{babyId}/avatar/url").onWrite(_imageResize2.default.handler);
 
-exports.memoriesResize = _firebaseFunctions2.default.database.ref("/memories/{memoryId}/files/{fileId}/url").onWrite(_imageResize2.default.handler);
+exports.coverImageResize = functions.database.ref("/babies/{babyId}/coverImage/url").onWrite(_imageResize2.default.handler);
+
+exports.memoriesResize = functions.database.ref("/memories/{memoryId}/files/{fileId}/url").onWrite(_imageResize2.default.handler);
