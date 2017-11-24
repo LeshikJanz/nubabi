36a01e8aa6cf6816f7c2684b8706329e81461221
treshaalesha@gmail.com <treshaalesha@gmail.com>
2017-11-24 07:19:59 +0300

    refactor: deleted commas from webpack.dev.config.js

--- platforms/web/config/webpack.config.dev.js
+++ platforms/web/config/webpack.config.dev.js
@@ -73,7 +73,7 @@ module.exports = {
     // https://github.com/facebookincubator/create-react-app/issues/253
     modules: ['node_modules', paths.appNodeModules].concat(
       // It is guaranteed to exist because we tweak it in `env.js`
-      process.env.NODE_PATH.split(path.delimiter).filter(Boolean),
+      process.env.NODE_PATH.split(path.delimiter).filter(Boolean)
     ),
     // These are the reasonable defaults supported by the Node ecosystem.
     // We also include JSX as a common component filename extension to support
@@ -90,7 +90,7 @@ module.exports = {
       web: path.resolve(__dirname, '../src/web'),
       'react-native-fetch-blob': path.resolve(
         __dirname,
-        '../config/react-native-fetch-blob',
+        '../config/react-native-fetch-blob'
       ),
     },
     plugins: [
@@ -158,7 +158,7 @@ module.exports = {
             exclude: [
               path.resolve(
                 __dirname,
-                '../../node_modules/react-native-fetch-blob',
+                '../../node_modules/react-native-fetch-blob'
               ),
             ],
             loader: require.resolve('babel-loader'),
