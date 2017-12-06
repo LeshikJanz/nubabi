e72c2aec47c8d76ae5cc07ff87a584a8bdba89a9
treshaalesha@gmail.com <treshaalesha@gmail.com>
2017-12-06 17:33:14 +0300

    refactor: refactor browse activities page

--- platforms/web/src/web/bundles/stimulation/components/FilteredActivities.js
+++ platforms/web/src/web/bundles/stimulation/components/FilteredActivities.js
@@ -1,6 +1,5 @@
 // @flow
 import React from 'react';
-import { Flex, Box } from 'grid-styled';
 import styled from 'styled-components';
 import { ActivityConnection } from 'core/types';
 import ActivityList from './ActivityList';
@@ -13,30 +12,10 @@ const Wrapper = styled.div`
   padding: 15px;
 `;
 
-const ListHeader = styled(Flex)`
-  margin: 34px 0 15px;
-`;
-
-const ListTitle = styled(Box)`
-  font-weight: normal;
-  font-size: 18px;
-  margin: 0;
-  color: ${props => props.theme.colors.open.black0};
-`;
-
-const FilteredActivities = ({ activities }: Props) => {
-  console.log('activities');
-  console.log(activities);
-
-  return (
-    <Wrapper>
-      <ListHeader justify="space-between" align="center">
-        <ListTitle is="h3">{`This Week's activities`}</ListTitle>
-      </ListHeader>
-
-      <ActivityList activities={activities.edges} />
-    </Wrapper>
-  );
-};
+const FilteredActivities = ({ activities }: Props) => (
+  <Wrapper>
+    <ActivityList activities={activities.edges} />
+  </Wrapper>
+);
 
 export default FilteredActivities;
