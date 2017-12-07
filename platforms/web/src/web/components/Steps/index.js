56155beab04d8ac6fe450e32a52160db56812c11
pavelpashkovsky <pavelpashkovsky@gmail.com>
2017-12-07 11:58:29 +0300

    feat: steps component

---
+++ platforms/web/src/web/components/Steps/index.js
@@ -0,0 +1,82 @@
+// @flow
+import React, { PureComponent } from 'react';
+import styled from 'styled-components';
+import { Section } from 'web/elements';
+import StepOne from './StepOne';
+import StepTwo from './StepTwo';
+
+type Props = {};
+
+type State = {
+  step: number,
+  stepOneInputs: Array<mixed>,
+  babyRadio: mixed,
+};
+
+const StepsContainer = styled(Section)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+class Steps extends PureComponent<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      step: 0,
+      stepOneInputs: [
+        {
+          type: 'name',
+          placeholder: 'Your full name',
+          value: 'Savannah Cooper',
+        },
+        {
+          type: 'email',
+          placeholder: 'Email address',
+          value: 'savannahcooper@gmail.com',
+        },
+        {
+          type: 'password',
+          placeholder: 'Nubabi password (at least 6 characters)',
+          value: 'password',
+        },
+      ],
+      babyRadio: {
+        current: 'girl',
+        variants: ['girl', 'boy'],
+      },
+    };
+  }
+
+  onNexStep = () => this.setState({ step: this.state.step + 1 });
+
+  onClickSignup = () => {};
+
+  radioOnChange = value => {
+    const babyRadio = Object.assign({}, this.state.babyRadio);
+    babyRadio.current = value;
+    this.setState({ babyRadio });
+  };
+
+  render() {
+    return (
+      <StepsContainer>
+        {this.state.step === 0 ? (
+          <StepOne
+            inputs={this.state.stepOneInputs}
+            onClickSignup={this.onClickSignup}
+            onNexStep={this.onNexStep}
+          />
+        ) : (
+          <StepTwo
+            radioOnChange={this.radioOnChange}
+            babyRadio={this.state.babyRadio}
+          />
+        )}
+      </StepsContainer>
+    );
+  }
+}
+
+export default Steps;
