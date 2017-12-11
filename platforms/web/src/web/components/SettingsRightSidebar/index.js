d1cd9a3343c7b44af9c97432d88b07c2a5274b8d
pavelpashkovsky <pavelpashkovsky@gmail.com>
2017-12-11 12:03:26 +0300

    feat: settings right sidebar

--- platforms/web/src/web/components/SettingsRightSidebar/index.js
+++ platforms/web/src/web/components/SettingsRightSidebar/index.js
@@ -4,13 +4,37 @@ import { Flex } from 'grid-styled';
 import styled from 'styled-components';
 
 const RightSidebar = styled(Flex)`
-  background-color: #fff;
   flex-grow: 1;
   max-width: 230px;
+  flex-direction: column;
+`;
+
+const Text = styled.span`
+  width: 100%;
+  color: ${props => props.theme.colors.secondary};
+  font-size: 12px;
+  font-weight: 300;
+  line-height: 17px;
+`;
+
+const Divider = styled.div`
+  width: 100%;
+  border-bottom: 1px solid ${props => props.theme.colors.border};
+  margin: 20px 0;
 `;
 
 const SettingsRightSidebar = () => {
-  return <RightSidebar>123</RightSidebar>;
+  return (
+    <RightSidebar>
+      <Text>©2017 Nubabi. About Us </Text>
+      <Text>• FAQ & Support • Contact Us • Log Out</Text>
+      <Divider />
+      <Text>
+        All rights reserved. Use of this website is regulated by our website
+        Terms of Use and Privacy Policy.
+      </Text>
+    </RightSidebar>
+  );
 };
 
 export default SettingsRightSidebar;
