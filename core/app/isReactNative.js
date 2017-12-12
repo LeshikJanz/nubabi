5fb9c517221cdf8e9b6e779086f5419a1bb75af3
Adrian Perez <adrian@adrianperez.org>
2017-12-12 13:17:49 +0100

    fix: `Promise.prototype.finally` is becoming ES, fix RN check

--- core/app/isReactNative.js
+++ core/app/isReactNative.js
@@ -4,9 +4,8 @@
 // RN 0.45 broke this test:
 // typeof navigator === 'object' && navigator.product === 'ReactNative';
 // so we check on added function to Promise prototype added by FBJS
-// This will fail if that ever becomes ES.
 
 // $FlowFixMe$
-const isReactNative = typeof Promise.prototype.finally !== 'undefined';
+const isReactNative = typeof Promise.prototype.done !== 'undefined';
 
 export default isReactNative;
