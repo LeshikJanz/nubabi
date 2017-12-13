0a6875f3dd9705f0a5685d3e965c5ddaf3d5b368
Adrian Perez <adrian@adrianperez.org>
2017-12-13 15:16:54 +0100

    refactor(native): remove react-native-fetch-blob

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
 // @flow
-const RNFetchBlob = require('react-native-fetch-blob').default;
 require('core/polyfills/process');
-
-const { Blob, XMLHttpRequest } = RNFetchBlob.polyfill;
-window.XMLHttpRequest = XMLHttpRequest;
-window.Blob = Blob;
-// XMLHttpRequest.setLog(3);
 
 import { AppRegistry } from 'react-native';
 import setup from './platforms/native/main';
-
-
 
 AppRegistry.registerComponent('NuBabiMobile', () => setup);
