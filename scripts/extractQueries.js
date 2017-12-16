8c2a5d470b3918b61a6c5328ac89ff9ba525c0a1
Adrian Perez <adrian@adrianperez.org>
2017-12-16 13:01:14 +0100

    build: extract queries

---
+++ scripts/extractQueries.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const fs = require('fs');
+const childProcess = require('child_process');
+const prettier = require('prettier');
+const gql = require('graphql-tag');
+
+console.log('Extracing queries...');
+childProcess.execSync('persistgraphql platforms/native --js --extension=js');
+console.log('Wrote extracted queries to extracted_queries.json');
+
+const queries = Object.keys(require('../extracted_queries.json'));
+
+queries.forEach(queryString => {
+  const query = gql`${queryString}`;
+  const queryName = query.definitions[0].name.value;
+  const queryFilename = path.resolve(
+    __dirname,
+    `../__generated__/${queryName}.graphql`,
+  );
+  console.log(`Writing ${queryFilename}`);
+  fs.writeFileSync(
+    queryFilename,
+    prettier.format(queryString, { parser: 'graphql' }),
+    'utf-8',
+  );
+});
