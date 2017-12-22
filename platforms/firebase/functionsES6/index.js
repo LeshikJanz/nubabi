61a409a5f527a52a233d195adde698f5c08b04f6
Adrian Perez <adrian@adrianperez.org>
2017-12-22 11:34:37 +0100

    chore: revert prettier on cloud functions, will add later

--- platforms/firebase/functionsES6/index.js
+++ platforms/firebase/functionsES6/index.js
@@ -1,23 +1,23 @@
-import * as functions from 'firebase-functions';
-import startCurriculum from './curriculum';
-import imageResize from './imageResize';
+import * as functions from "firebase-functions";
+import startCurriculum from "./curriculum";
+import imageResize from "./imageResize";
 
 exports.startCurriculum = functions.database
-  .ref('/babies/{babyId}')
+  .ref("/babies/{babyId}")
   .onWrite(startCurriculum.handler);
 
 exports.userAvatarResize = functions.database
-  .ref('/users/{userId}/avatar/url')
+  .ref("/users/{userId}/avatar/url")
   .onWrite(imageResize.handler);
 
 exports.babyAvatarResize = functions.database
-  .ref('/babies/{babyId}/avatar/url')
+  .ref("/babies/{babyId}/avatar/url")
   .onWrite(imageResize.handler);
 
 exports.coverImageResize = functions.database
-  .ref('/babies/{babyId}/coverImage/url')
+  .ref("/babies/{babyId}/coverImage/url")
   .onWrite(imageResize.handler);
 
 exports.memoriesResize = functions.database
-  .ref('/memories/{memoryId}/files/{fileId}/url')
+  .ref("/memories/{memoryId}/files/{fileId}/url")
   .onWrite(imageResize.handler);
