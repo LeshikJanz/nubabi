404ca2b8645b9d8320fa0c91396fef02d6167961
Adrian Perez <adrian@adrianperez.org>
2017-12-22 11:36:32 +0100

    chore: revert formatting changes to platforms/server

--- platforms/server/graphql/helpers/loaders.js
+++ platforms/server/graphql/helpers/loaders.js
@@ -4,11 +4,9 @@ import * as babies from '../connectors/babiesConnector';
 
 const genBabyLoader = (token, loadFn) => {
   return new DataLoader(keys => {
-    return Promise.all(
-      keys.map(id => {
-        return loadFn(token, id);
-      }),
-    );
+    return Promise.all(keys.map(id => {
+      return loadFn(token, id);
+    }));
   });
 };
 
@@ -17,7 +15,7 @@ export function genLoaders(token: string, connectors) {
     // skillArea: new DataLoader(ids => genSkillAreas(token, ids))
     skillArea: genBabyLoader(token, babies.getSkillArea),
     expert: genBabyLoader(token, babies.getExpert),
-  };
+  }
 }
 
 export function genSkillAreas(token: string, ids: Array<string>) {
